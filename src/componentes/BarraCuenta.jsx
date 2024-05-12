e90245305e6e87b65a7737f350839abf7a9cd305
Cliff-Vic <cliffvicunaguerrero@gmail.com>
2024-05-11 23:03:04 -0500

    instalacion de tailwind

---
+++ src/componentes/BarraCuenta.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function BarraCuenta() {
+  return (
+    <aside id="BarraNavegacionAdmin">
+        <p id="BNANombre">Admin</p>
+        <ul id="BNAListado">
+            <li id="BNADasboard">Dasboard</li>
+            <li id="BNAUsuariosRegistrados">Usuarios registrados</li>
+            <li id="BNAProductos">Productos</li>
+            <li id="BNAOrdenes">Órdenes</li>
+            <li id="BNAProductosMV">Productos más vendidos</li>
+            <li id="BNASeries">Series</li>
+        </ul>
+    </aside>
+  )
+}
+
+export default BarraCuenta
