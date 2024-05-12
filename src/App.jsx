a9e1c97e8642d52296ae2bb61334ca6e8a18822d
Cliff-Vic <cliffvicunaguerrero@gmail.com>
2024-05-11 19:58:23 -0500

    Creacion de las carpetas para las vistas

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
-import BarraNavegacion from "./componentes/BarraNavegacion";
-import PaginaError from "./paginas/Basicos/PaginaError"
-import UsuariosRegistrados from "./paginas/Administrador/UsuariosRegistrados";
-import Ordenes from "./paginas/Administrador/Ordenes";
-import Principal from "./paginas/Principal";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Principal />
-  },
-  {
-    path: "/Administrador/UsuariosRegistrados",
-    element: <UsuariosRegistrados />,
-  },
-  {
-    path: "/Administrador/Ordenes",
-    element: <Ordenes />,
-  },
-  {
-    path: "*",
-    element: <PaginaError />,
-  },
-]);
+import { useState } from 'react'
 
 function App() {
   return (
-    <div className='App'>
-      <RouterProvider router={router} />
-    </div>
+    <div className='App'></div>
+    // Poner JSX
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider, Route, Link} from "react-router-dom";
+import Principal from './vistas/Pagina/Principal/Principal.jsx'
+import PaginaError from './componentes/PaginaError.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Principal />,
+    errorElement: <PaginaError />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </React.StrictMode>,
 )
