0bd27ac1c11f8e18ba887d519b0f3c78ea81850a
GasperVC <cliffvicunaguerrero@gmail.com>
2024-05-21 20:06:39 -0500

    Funiona correctamente el localStorage, falta el detalle de usuario, ordenes y detalle de ordenes, tambien ya funciona que al cambiar alguna informacion este permanece tras actualizar o cambiar de pagina

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ import PaisData from './data/Pais.json';
 import PersonaData from './data/Persona.json';
 import ProductoData from './data/Producto.json';
 import ProvinciaData from './data/Provincia.json';
-import UsuarioData from './data/Direccion.json';
+import UsuarioData from './data/Usuario.json';
 import UsuarioDireccionData from './data/UsuarioDireccion.json';
 
 
@@ -164,21 +164,28 @@ const router = createBrowserRouter([
   },
 ]);
 
+  // Guardar datos en el Local Storage solo si no existen
+  const guardarEnLocalStorage = (clave, datos) => {
+    if (!localStorage.getItem(clave)) {
+        localStorage.setItem(clave, JSON.stringify(datos));
+    }
+  };  
+
   // Cargar los datos JSON al localStorage si no están ya almacenados
-  localStorage.setItem("departamentos", JSON.stringify(DepartamentoData));
-  localStorage.setItem("detalles", JSON.stringify(DetalleData));
-  localStorage.setItem("direcciones", JSON.stringify(DireccionData));
-  localStorage.setItem("distritos", JSON.stringify(DistritoData));
-  localStorage.setItem("envios", JSON.stringify(EnvioData));
-  localStorage.setItem("mediosPago", JSON.stringify(MedioPagoData));
-  localStorage.setItem("ordenes", JSON.stringify(OrdenData));
-  localStorage.setItem("pagos", JSON.stringify(PagoData));
-  localStorage.setItem("paises", JSON.stringify(PaisData));
-  localStorage.setItem("personas", JSON.stringify(PersonaData));
-  localStorage.setItem("productos", JSON.stringify(ProductoData));
-  localStorage.setItem("provincias", JSON.stringify(ProvinciaData));
-  localStorage.setItem("usuarios", JSON.stringify(UsuarioData));
-  localStorage.setItem("usuarioDireccion", JSON.stringify(UsuarioDireccionData));
+  guardarEnLocalStorage("departamentos", DepartamentoData);
+  guardarEnLocalStorage("detalles", DetalleData);
+  guardarEnLocalStorage("direcciones", DireccionData);
+  guardarEnLocalStorage("distritos", DistritoData);
+  guardarEnLocalStorage("envios", EnvioData);
+  guardarEnLocalStorage("mediosPago", MedioPagoData);
+  guardarEnLocalStorage("ordenes", OrdenData);
+  guardarEnLocalStorage("pagos", PagoData);
+  guardarEnLocalStorage("paises", PaisData);
+  guardarEnLocalStorage("personas", PersonaData);
+  guardarEnLocalStorage("productos", ProductoData);
+  guardarEnLocalStorage("provincias", ProvinciaData);
+  guardarEnLocalStorage("usuarios", UsuarioData);
+  guardarEnLocalStorage("usuarioDireccion", UsuarioDireccionData);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
