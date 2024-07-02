8ac110accaaa959b70156790d85ba75440013df6
Mauri R <ramirezmauricio39@gmail.com>
2024-07-02 04:59:23 -0500

    cambio en el menu ordenes recientes

--- src/componentes/Usuario/Menu.jsx
+++ src/componentes/Usuario/Menu.jsx
@@ -1,31 +1,75 @@
-import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Menu_SVG from '../../svg/Menu_SVG'
+import { styled } from '@mui/material/styles';
+import { useState } from 'react';
 // Menu en la parte izquierda de la pagina main del usuario
-function Menu() {
-  //UseNavigate para manejar entre direcciones
-  const navigate = useNavigate()
-  function handleClickRecently () {
-    navigate('/usuarios/main')
-  }
-  function handleClickRegister () {
-    navigate('/Usuario/DatosRegistro')
-  }
-  function handleClickChangePassword () {
-    navigate('/Usuario/CambiarPassword')
-  }
-  return(
-    <>
-      <aside className=" bg-[#0c2941] rounded my-8 mx-2 w-52">
-        <h5 className="font-bold ml-4 mt-6 m-4 text-2xl text-white">Mi Cuenta</h5>
-        <ol className="flex flex-col items-center">
-          <li onClick={handleClickRecently}
-           className=" mb-2 text-lg text-gray-400 hover:text-white"><button>Ordenes Recientes</button></li>
-          <li onClick={handleClickRegister}
-          className=" mb-2 text-lg text-gray-400 hover:text-white"><button>Datos de Registro</button></li>
-          <li onClick={handleClickChangePassword}
-          className="text-lg text-gray-400 hover:text-white"><button>Cambiar Password</button></li>
-        </ol>
-      </aside>
-    </>
-  )
+const CustomListItemButton = styled(ListItemButton)({
+  '&:hover': {
+    borderLeft: '3px solid green', // borde color verde
+    backgroundColor: '#f0f0f0', // color gris claro
+  },
+});
+
+export default function AnchorTemporaryDrawer() {
+  const [state, setState] = useState({left: false});
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+};
+
+const list = () => (
+  <Box
+    sx={{ width: 250, backgroundColor: '#fff', color: '#212529', height: '100%' }}
+    role="presentation"
+    onClick={toggleDrawer('left', false)}
+    onKeyDown={toggleDrawer('left', false)}
+  >
+    <div style={{ padding: '16px', borderBottom: '1px solid #e0e0e0'  }}>
+      <p style={{ fontSize: '1.25rem', fontWeight: 'bold' }}>Mi cuenta</p>
+    </div>
+    <Divider sx={{ backgroundColor: '#495057' }} />
+    <List>
+      {[
+        { text: 'Ordenes Recientes', link: '/usuarios/main' },
+        { text: 'Datos de Registro', link: '/Usuario/DatosRegistro'},
+        { text: 'Cambiar Password', link: '/Usuario/CambiarPassword' }, 
+      ].map((item) => (
+        <Link to={item.link}>
+          <ListItem key={item.text} disablePadding>          
+            <CustomListItemButton >            
+              <ListItemText primary={item.text}/>
+            </CustomListItemButton>          
+          </ListItem>
+        </Link> 
+      ))}
+    </List>
+  </Box>
+);
+
+return (
+  <div>
+    <Button onClick={toggleDrawer('left', true)} sx={{width: 25, backgroundColor: '#fff', border: "solid, green", height: '100%'}}>
+      <Menu_SVG width="24px" height="24px" fill="LightGreen" />
+    </Button>
+    <Drawer
+      anchor="left"
+      open={state.left}
+      onClose={toggleDrawer('left', false)}>
+      {list()}
+    </Drawer>
+  </div>
+);
 }
-export default Menu
+
