7dcad98bb5238c486ec91ca9820f60b06f2ccef9
Nicolas Lopez <lopezh.nicolased@gmai.com>
2024-07-14 18:19:41 -0500

    solucion pantalla blanca

--- src/vistas/Administrador/Series/AgregarSerie.jsx
+++ src/vistas/Administrador/Series/AgregarSerie.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useHistory } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import ImageUploader from './ImageUploader';
 import HVacio from '../../../componentes/Header/HVacio';
 import Footer from '../../../componentes/Footer';
